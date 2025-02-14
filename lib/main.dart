a0e7f7f8fa3427d049136018b0968f5b5296c74f
Admin <devangana1910@gmail.com>
2025-02-14 22:17:15 +0530

    web

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,31 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv
 import 'package:sample/constants.dart';
 import 'package:sample/pages/splashscreen.dart';
 import 'package:sample/services/notif_handler.dart';
 
 import 'pages/login.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
-  String apiKey = dotenv.env['API_KEY']!;
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load .env file (Web requires specifying the correct path)
+  await dotenv.load(fileName: "assets/.env");
+
+  String apiKey = dotenv.env['API_KEY'] ?? "";
+
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: API_KEY,
+      apiKey: apiKey,
       appId: Constants.appId,
       messagingSenderId: Constants.messagingSenderId,
       projectId: Constants.projectId,
     ),
   );
 
-
   GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 
   NotificationHandler notificationHandler = NotificationHandler(navigatorKey: navigatorKey);
   notificationHandler.initialize();
@@ -46,7 +48,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
       navigatorObservers: <NavigatorObserver>[observer],
-      home:  SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
